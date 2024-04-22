a78c3f11db7812fe3e9bd2b054b374a41e4d1f6c
Jonister <magnetl12.21@gmail.com>
2024-04-22 19:51:07 +0300

    test 10

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -2,7 +2,7 @@ import '../common/generall.scss';
 import '../common/reset.scss';
 const Wrapper = () => {
 	return (
-		<div>BLack neger its max</div>
+		<div>!!!!!! BLack neger its max !!!!!!</div>
 	);
 };
 export default Wrapper;
