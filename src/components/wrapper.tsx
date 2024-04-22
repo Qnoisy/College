676aa191754231f26d6ba967b0c9571b3f4176c0
Aveskulov <WenJoymk3@gmail.com>
2024-04-22 18:14:00 +0200

    test4

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,6 +1,13 @@
 import '../common/generall.scss';
 import '../common/reset.scss';
 const Wrapper = () => {
-	return <div>Привіт світ!!!!!!</div>;
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro
+			nobis, eius repellendus, magnam nostrum mollitia molestias libero ipsam
+			eligendi quis assumenda voluptas obcaecati placeat quae ab at, voluptates
+			laboriosam?
+		</div>
+	);
 };
 export default Wrapper;
