bf7ee7919febfd750b3e7dd2edaaf6fd6904bba3
Aveskulov <WenJoymk3@gmail.com>
2024-04-22 17:31:45 +0200

    start v2

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,6 +1,5 @@
-import './common/generall.scss';
-import './common/reset.scss';
-
+import '../common/generall.scss';
+import '../common/reset.scss';
 const Wrapper = () => {
 	return <div>it's working</div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import Wrapper from './components/wrapper';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<Wrapper />
 	</React.StrictMode>
 );
 
