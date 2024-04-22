0a969fd709dbe40583c820f3bcc24de277f291dd
Jonister <magnetl12.21@gmail.com>
2024-04-22 19:28:09 +0300

    test5

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -2,12 +2,7 @@ import '../common/generall.scss';
 import '../common/reset.scss';
 const Wrapper = () => {
 	return (
-		<div>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro
-			nobis, eius repellendus, magnam nostrum mollitia molestias libero ipsam
-			eligendi quis assumenda voluptas obcaecati placeat quae ab at, voluptates
-			laboriosam?
-		</div>
+		<div>Максім лисий хуй рижий</div>
 	);
 };
 export default Wrapper;
