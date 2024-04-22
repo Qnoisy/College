42bdd80936a8466eddcfe7d4d09481e31ad2f714
Jonisterr <magnetl12.21@gmail.com>
2024-04-22 19:06:40 +0300

    test3

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -1,6 +1,6 @@
 import '../common/generall.scss';
 import '../common/reset.scss';
 const Wrapper = () => {
-	return <div>Привіт світ</div>;
+	return <div>Привіт світ!!!!!!</div>;
 };
 export default Wrapper;
