b162058107626409fda398b54029292aab4a4a93
Mstitel <muliarchuk.d@chpt.edu.ua>
2024-04-22 19:33:29 +0300

    test6

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -2,7 +2,7 @@ import '../common/generall.scss';
 import '../common/reset.scss';
 const Wrapper = () => {
 	return (
-		<div>Максім лисий хуй рижий</div>
+		<div>BLack tea</div>
 	);
 };
 export default Wrapper;
