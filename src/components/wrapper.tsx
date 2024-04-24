aa435e8e96cfea5c64c21fcc377ff4b1aa1dd2aa
Jonister <magnetl12.21@gmail.com>
2024-04-24 11:54:26 +0300

    lastversion

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -5,9 +5,6 @@ import { Footer } from './Footer';
 import { Header } from './Header';
 import { Main } from './Main';
 const Wrapper = () => {
-<<<<<<< HEAD
-	return <div>!!!!!! BLack neger its max !!!!!!</div>;
-=======
 	return (
 		<div className='wrapper'>
 			<div className='header'>
@@ -27,6 +24,5 @@ const Wrapper = () => {
 			</div>
 		</div>
 	);
->>>>>>> main
 };
 export default Wrapper;
