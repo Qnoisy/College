dac432e25cbe3c68b084ea554a3dfe7b4e65bfcc
Aveskulov <WenJoymk3@gmail.com>
2024-04-24 11:17:23 +0200

    routing

---
+++ src/components/Publichne/Publichne.tsx
@@ -0,0 +1,7 @@
+import styles from './Publichne.module.scss';
+
+interface PublichneProps {}
+
+export const Publichne = ({}: PublichneProps) => {
+	return <div className={styles.publichne}>Publichne Component</div>;
+};
