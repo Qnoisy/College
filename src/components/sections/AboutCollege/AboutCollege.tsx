29c1e3647692b700fe58bc43f5096c03d0b4913a
Aveskulov <WenJoymk3@gmail.com>
2024-05-14 20:48:56 +0200

    about

---
+++ src/components/sections/AboutCollege/AboutCollege.tsx
@@ -0,0 +1,7 @@
+import styles from './AboutCollege.module.scss';
+
+interface AboutCollegeProps {}
+
+export const AboutCollege = ({}: AboutCollegeProps) => {
+	return <div className={styles.aboutCollege}>AboutCollege Component</div>;
+};
