3ff7dd833be4f4ba53f3a6bdb67eb7c2f3be22c2
Aveskulov <WenJoymk3@gmail.com>
2024-05-14 22:46:05 +0200

    upMenu

--- src/routes/under-routes/Entry-company/Entry-company.tsx
+++ src/routes/under-routes/Entry-company/Entry-company.tsx
@@ -1,5 +1,5 @@
 import Page from '../../../Page/Page';
-import DropdownList from '../../../components/DropdownList/DropdownList';
+import DropUpMenu from '../../../components/DropUpMenu/DropUpMenu';
 import DemoBlock from '../../../components/sample/DemoBlock/DemoBlock';
 import Title from '../../../components/sample/Title/Title';
 
@@ -18,16 +18,25 @@ export const Entrycompany = () => {
 	];
 
 	const list = [
-		{ name: 'Обзор', path: '/accreditation/overview' },
-		{ name: 'Документы', path: '/accreditation/documents' },
-		{ name: 'Контакты', path: '/accreditation/contacts' },
+		{
+			text: 'Правила прийому до черкаського політехнічного фахового коледжу для здобуття освітньо-професійного ступеня фахового молодшого бакалаρα у 2024 році',
+			url: 'https://drive.google.com/file/d/1zqZEcLOUcjpOKR6c6DKZ-uu6mnPQ0sVW/view?usp=sharing',
+		},
+		{
+			text: 'Положення про приймальну комісію черкаського політехнічного фахового коледжу у 2024 році',
+			url: 'https://drive.google.com/file/d/1zqZEcLOUcjpOKR6c6DKZ-uu6mnPQ0sVW/view?usp=sharing',
+		},
+		{
+			text: 'Наказ про зарахування на навчання денної форми здобуття освіти за кошти фізичних та/або юридичних осіб на основі БСО (9 класів)',
+			url: 'https://drive.google.com/file/d/1C-xRkuAxPaR4LYziaBs1vAYEruFUqUQT/view?usp=sharing',
+		},
 	];
 
 	const accreditationContent = (
 		<div>
 			<Title text='Приймальна комісія' />
 			<hr className='hr-style' />
-			<DropdownList options={list}/> 
+			<DropUpMenu links={list} name={'Правила прийому'} />
 			<hr className='hr-style' />
 		</div>
 	);
