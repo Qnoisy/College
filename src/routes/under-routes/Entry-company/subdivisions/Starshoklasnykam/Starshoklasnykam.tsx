76d068c84038892793fe5e81d3ddb1714b0b978b
Jonister <magnetl12.21@gmail.com>
2024-05-21 18:59:43 +0300

    blin3

--- src/routes/under-routes/Entry-company/subdivisions/Starshoklasnykam/Starshoklasnykam.tsx
+++ src/routes/under-routes/Entry-company/subdivisions/Starshoklasnykam/Starshoklasnykam.tsx
@@ -2,16 +2,13 @@ import Page from '../../../../../Page/Page';
 import Block from '../../../../../components/sample/Block/Block';
 import Title from '../../../../../components/sample/Title/Title';
 
-<<<<<<< HEAD:src/routes/under-routes/Entry-company/subdivisions/Nashi-spetsialnosti/Nashi-spetsialnosti.tsx
- const Nashispetsialnosti = () => {
-	const nashispetsialnostiContent = (
-=======
 export const Starshoklasnykam = () => {
+	
 	const starshoklasnykamContent = (
->>>>>>> 53d60207cb7f7d55c54211b6c40a0347cb02cfdd:src/routes/under-routes/Entry-company/subdivisions/Starshoklasnykam/Starshoklasnykam.tsx
 		<div>
 			<Title text='Старшокласникам' />
 			<hr className='hr-style' />
+			
 			<hr className='hr-style' />
 		</div>
 	);
@@ -22,5 +19,3 @@ export const Starshoklasnykam = () => {
 		</Page>
 	);
 };
-
-export default Nashispetsialnosti
